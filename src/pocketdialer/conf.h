d2f63cae67389b892833cf82f119b4534b9618e3
nate <nate.wilgus@proton.me>
2023-11-15 15:38:48 -0600

    11/15/23

--- src/pocketdialer/conf.h
+++ src/pocketdialer/conf.h
@@ -1,20 +1,18 @@
-#ifndef conf
-#define conf
-
 //Pin Assignment
-const u_int8_t = EN 16; //Enable Pin
-const u_int8_t = MS1 17; //Microstepping pin 1
-const u_int8_t = MS2 18; //Microstepping pin 2
-const u_int8_t = MS3 19; //Microstepping pin 3
-const u_int8_t = RST 21; //Reset pin
-const u_int8_t = SLP 22; //sleep pin
-const u_int8_t = DirPin 23; //direction pin
-const u_int8_t = StepPin 25; //step pin
-const u_int8_t = switchPin 26; //
+const uint8_t EN = 16; //Enable Pin
+const uint8_t MS1 = 17; //Microstepping pin 1
+const uint8_t MS2 = 18; //Microstepping pin 2
+const uint8_t MS3 = 19; //Microstepping pin 3
+const uint8_t RST = 21; //Reset pin
+const uint8_t SLP = 22; //sleep pin
+const uint8_t DirPin = 23; //direction pin
+const uint8_t StepPin = 25; //step pin
+const uint8_t switchPin = 26; //pin for zeroing switch
  
  
-// motor speed configuration
-const u_int8_t = microStepsFactor 8;
-const u_int8_t = maxStepperSpeed 4000;
-const u_int8_t = inversionPause 50;
-const u_int8_t = motorInterfaceType 1;
+// motor configuration
+const uint8_t microStepsFactor = 8;
+const uint8_t maxStepperSpeed = 4000;
+const uint8_t inversionPause = 50;
+//const uint8_t motorInterface = 1;
+float acceleration = 40000;
--- src/pocketdialer/pocketdialer.ino
+++ src/pocketdialer/pocketdialer.ino
@@ -1,26 +1,29 @@
 #include <AccelStepper.h>
-//#include <conf.h>
+#include "conf.h"
 
-//Pin Assignment
-const u_int8_t = EN 16; //Enable Pin
-const u_int8_t = MS1 17; //Microstepping pin 1
-const u_int8_t = MS2 18; //Microstepping pin 2
-const u_int8_t = MS3 19; //Microstepping pin 3
-const u_int8_t = RST 21; //Reset pin
-const u_int8_t = SLP 22; //sleep pin
-const u_int8_t = DirPin 23; //direction pin
-const u_int8_t = StepPin 25; //step pin
-const u_int8_t = switchPin 26 //
+// //Pin Assignment
+// const uint8_t EN = 16; //Enable Pin
+// const uint8_t MS1 = 17; //Microstepping pin 1
+// const uint8_t MS2 = 18; //Microstepping pin 2
+// const uint8_t MS3 = 19; //Microstepping pin 3
+// const uint8_t RST = 21; //Reset pin
+// const uint8_t SLP = 22; //sleep pin
+// const uint8_t DirPin = 23; //direction pin
+// const uint8_t StepPin = 25; //step pin
+// const uint8_t switchPin = 26; //pin for zeroing switch
  
  
-// motor speed configuration
-const u_int8_t = microStepsFactor 8
-const u_int8_t = maxStepperSpeed 4000
-const u_int8_t = inversionPause 50
-const u_int8_t = motorInterfaceType 1
+// // motor configuration
+// const uint8_t maxStepperSpeed = 4000;
+// const uint8_t inversionPause = 50;
 
+//Runtime vars
+uint8_t DialPos = 0;    //current dial postition
+uint8_t DiskPos[] = {0, 0, 0};    //array of current disk positions
 
-AccelStepper stepper = AccelStepper(motorInterfaceType, StepPin, DirPin);
+//initialize motor construct
+AccelStepper motor = AccelStepper(1, StepPin, DirPin);
+
 
 
 
@@ -31,9 +34,19 @@ AccelStepper stepper = AccelStepper(motorInterfaceType, StepPin, DirPin);
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(115200);
+
+  motor.setMaxSpeed(maxStepperSpeed);
+  motor.setAcceleration(acceleration);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  for (uint8_t i = 0, i < 99, i++) {
+
+    
+  }
 
 }
